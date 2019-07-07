5865150daba675ce047d6fb9558e6a86b34e8ab8
Sofiene Bourghes <sofiene.bourghes@epita.fr>
2019-07-07 15:49:51 -0700

    add Main Spark Data analyse

--- SPARK-KAFKA/src/main/scala/spark/Main.scala
+++ SPARK-KAFKA/src/main/scala/spark/Main.scala
@@ -4,18 +4,67 @@ import org.apache.spark._
 import org.apache.spark.SparkContext._
 // Implicits that add functions to the SparkContext & RDDs.
 import com.datastax.spark.connector._
+import org.apache.spark.rdd.RDD
 
 object Main extends App {
 
-  val sparkMaster = "local[*]"
-  val cassandraHost = "localhost"
-  val conf = new SparkConf(true)
-    .set("spark.cassandra.connection.host", cassandraHost)
-    val sc = new SparkContext(sparkMaster, "BasicQueryCassandra", conf)
-    // entire table as an RDD
-    // assumes your table test was created as CREATE TABLE test.kv(key text PRIMARY KEY, value int);
-    val data = sc.cassandraTable("isd_car_data", "raw_car_data")
+  @SerialVersionUID(1L)
+  sealed trait CarModel extends Serializable
 
-    //val df = spark.read.format("org.apache.spark.sql.cassandra").options(Map( "table" -> "raw_car_data", "keyspace" -> "ids_car_data")).load() // This Dataset will use a spark.cassandra.input.size of 128
-    data.foreach(car => System.out.println(car))
+  case class CarData (
+    val id : String,
+    val location : String,
+    val speed : Float,
+    val acceleration : Float,
+    val fuel : Float,
+    val enginetemp : Float,
+    val nextstep : String) extends CarModel
+
+  object CarData {
+    def apply(array: Array[String]): CarData = {
+      CarData(
+        id = array(0),
+        location = array(1),
+        speed = array(2).toFloat,
+        acceleration = array(3).toFloat,
+        fuel = array(4).toFloat,
+        enginetemp = array(5).toFloat,
+        nextstep = array(6))
+    }
+  }
+
+  def loadData() : RDD[CarData] = {
+    val sparkMaster = "local[*]"
+    val cassandraHost = "localhost"
+    val conf = new SparkConf(true)
+      .set("spark.cassandra.connection.host", cassandraHost)
+      val sc = new SparkContext(sparkMaster, "BasicQueryCassandra", conf)
+      sc.setLogLevel("ERROR")
+      // entire table as an RDD
+      // assumes your table test was created as CREATE TABLE test.kv(key text PRIMARY KEY, value int);
+      val data = sc.cassandraTable[CarData]("isd_car_data", "raw_car_data")
+      data
+  }
+
+  def dataToString(data : RDD[CarData]) = {
+    data.map(x => x.toString()).fold("")((s1, s2) => s1 + "\n" + s2)
+  }
+
+  def slowestCar(data : RDD[CarData]) = {
+    data.sortBy(_.speed).take(5)
+  }
+
+  def slowestCarWithEngineAlert(data : RDD[CarData]) = {
+    slowestCar(data).filter(x => x.enginetemp > 99.0)
+  }
+
+  val data = loadData()
+  System.out.println("Full DataBase : ")
+  System.out.println(dataToString(data))
+
+  System.out.println("Slowest Car : ")
+  slowestCar(data).foreach(x => System.out.println(x))
+
+  System.out.println("Slowest Car with engine alert: ")
+  slowestCarWithEngineAlert(data).foreach(x => System.out.println(x))
 }
